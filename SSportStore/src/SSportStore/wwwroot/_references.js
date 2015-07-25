94a3f053f8843eca8142b6ce93d42c29a3f34f9a
Justin <joehlmann@hotmail.com>
2015-07-25 14:29:55 +1000

    Http & Error

---
+++ SSportStore/src/SSportStore/wwwroot/_references.js
@@ -0,0 +1,10 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="controllers/productlistcontroller.js" />
+/// <reference path="controllers/sportsstore.js" />
+/// <reference path="js/site.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/angular/angular.js" />
+
+
+
